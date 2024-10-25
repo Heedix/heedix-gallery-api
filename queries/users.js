e3459e24615dbece3e90c4885e7d9e0a99a193b4
Jannis Austgen <jannis.austgen@gmail.com>
2024-10-25 14:03:15 +0200

    - added Database functionality to register function - updated Database with constraints and uuid as userid

--- queries/users.js
+++ queries/users.js
@@ -1,15 +1,36 @@
 const {request, response} = require("express");
-const dataSource = require('../data-source');
-const pool = dataSource;
+const pool = require('../data-source');
 
 const getUserByUsername = (username) => {
     return pool.query('SELECT * FROM users WHERE username = $1', [username])
         .then(results => results.rows)
         .catch(error => {
-            throw error // Fehlerbehandlung kann angepasst werden
+            throw error
         })
 }
 
+const addUserToDb = async (email, username, password) => {
+    const client = await pool.connect();
+    try {
+        const query = `
+            INSERT INTO users (email, username, password)
+            VALUES ($1, $2, $3)
+            RETURNING userid
+        `;
+        const values = [email, username, password];
+        const result = await client.query(query, values);
+
+        console.log("User added with ID:", result.rows[0].userid);
+        return result.rows[0];
+    } catch (error) {
+        //console.error("Error adding user:", error);
+        throw error;
+    } finally {
+        client.release();
+    }
+};
+
 module.exports = {
-    getUserByUsername
+    getUserByUsername,
+    addUserToDb
 }
