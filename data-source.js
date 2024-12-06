a5339d9c3e224707a2edd9d246ac71508393afc1
Jannis Austgen <jannis.austgen@gmail.com>
2024-12-06 19:59:03 +0100

    - modified docker-image.yml

--- data-source.js
+++ data-source.js
@@ -15,12 +15,4 @@ const pool = new Pool({
     port: DB_PORT,
 })
 
-console.log(pool)
-console.log(process.env)
-console.log('DB_USER: ' + DB_USER)
-console.log('DB_HOST: ' + DB_HOST)
-console.log('DB_DATABASE: ' + DB_DATABASE)
-console.log('DB_PASSWORD: ' + DB_PASSWORD)
-console.log('DB_PORT: ' + DB_PORT)
-
 module.exports = pool
